64a57949ca9e564f1b72511b86a331615ea2c7d9
Kelsey Motley <kelsey@kelseyvaughn.com>
2015-03-30 07:10:39 -0400

    responsivness complete

---
+++ js/views/PaneView.js
@@ -0,0 +1,59 @@
+// _________________________________________________________________________ PaneView
+main.views.PaneView = Backbone.View.extend({
+	ACTIVE_CLASS: "pane-active",
+	PREPARED_CLASS: "pane-prepared",
+	FULL_SCREEN_CLASS: "full-screen",
+	DEACTIVATE: "deactivate",
+	ACTIVATE: "activate",
+	RESIZE: "resize",
+	offset: 0,
+	id: "",
+	_route: "",
+	paneActivationData: [],
+    // ----------------- initialize
+    initialize: function() {
+        console.log("PaneView ---- initialize");
+    },
+    // ----------------- render
+    render: function(eventName) {
+	    var self = this;
+	    if(this.beforeRender) this.beforeRender();
+        console.log("PaneView ---- render");
+        //create a view for each pane
+        setTimeout(function(){
+	         self.posize();
+        }, 100);
+        return this;
+	},
+	// ----------------- posize
+    posize: function() {
+    	console.log("PaneView ---- posize ----------------- ");
+        if(this.beforePosize) this.beforePosize();
+		if( $(this.el).hasClass(this.FULL_SCREEN_CLASS)){
+			var to_h = $(window).outerHeight();
+			var min_h = $(this.el).css('min-height');
+			if(to_h < min_h) to_h = min_h;
+			$(this.el).height( to_h );
+		} 
+    },
+    // ----------------- activate
+    activate: function() {
+    	console.log("activate ----------------- this.id = " + this.id);
+	    $(this.el).addClass(this.ACTIVE_CLASS);
+	    if(this.beforeActivate) this.beforeActivate();
+	    if(main.router.navigate && !main.router.autoScrolling)  main.router.navigate(this._route, {trigger: false});
+	    $(this.el).trigger(this.ACTIVATE, [this.id]);
+    }, 
+    // ----------------- deactivate
+    deactivate: function() {
+	    console.log("deactivate ----------------- this.id = " + this.id);
+    	$(this.el).removeClass(this.ACTIVE_CLASS);
+	    if(this.beforeDeactivate) this.beforeDeactivate();
+	    $(this.el).trigger(this.DEACTIVATE);
+    },
+    // ----------------- prepare
+    prepare: function() {
+		$(this.el).addClass(this.PREPARED_CLASS);
+    	if(this.beforePrepare) this.beforePrepare();
+    }
+});
